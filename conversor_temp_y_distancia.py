d38a80318da0ae572cc67d0c9e1e87e4a01f4922
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-10-08 17:42:23 -0300

    Add files via upload

---
+++ conversor_temp_y_distancia.py
@@ -0,0 +1,18 @@
+#Conversor Temperaturas
+kelvin=int(input("entre la temperatura en Kelvin "))
+celsius=(kelvin - 273)
+print(("esta es la temperatura convertida a Celsius "),(celsius))
+fahrenheit=int(input("entre la temperatura en Fahrenheit "))
+celsius2= (fahrenheit -32*5/9)
+print("esta es la temperatura convertida de Fahrenheit a Celsius ", (celsius2))
+
+#conversor de distancias
+metro=int(input("Escriba la distancia en metros "))
+centimetro=(metro*100)
+print(("la distancia calculada en centimetros es "),(centimetro))
+
+
+#conversor kilometros a metros
+kilometro=int(input("Escriba la distancia en kilometros "))
+metros=(kilometro*1000)
+print(("La Distancia calculada en metros es "),(metros))
